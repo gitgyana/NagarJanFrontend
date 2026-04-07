3a1661868fd4139849b6bb4d6bacd06a034eb845
Gyana Priyadarshi <gyana.career@gmail.com>
2026-04-07 16:42:13 +0530

    Add admin route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadChildren: () => import("./feature/admin/admin.route").then((m) => m.ADMIN_ROUTES),
+    }
+];
---
+++ src/app/feature/admin/admin.route.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import {Admin} from './admin';
+
+export const ADMIN_ROUTES: Routes = [
+    {
+        'path': '',
+        component: Admin,
+        data: { title: 'Admin Dashboard' },
+    }
+]
